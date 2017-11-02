e8af69150aa1e328a497dd4a17b9e82035d35094
loet <loet@swissonline.ch>
2017-11-02 11:52:19 +0100

    added working (?) test config, just playing around...

--- src/components/my-component.ts
+++ src/components/my-component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-component',
-  template: `<div class="my-component">I'm a special snowflake, version 0.0.14</div>`
+  template: `<div><div class="my-component">I'm a special snowflake, version 0.0.14</div></div>`
 })
 export class MyComponent {
   constructor() {}
