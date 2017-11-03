cf2a3b2bf032eaa20bc042e7a70e8e96974d5da9
loet <loet@swissonline.ch>
2017-11-03 17:03:49 +0100

    added testcomponent

--- src/providers/my-provider.ts
+++ src/providers/my-provider.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class MyProvider {
   myMethod() {
-    console.log("I'm afraid I can't do that in version 0.0.14");
+    console.log("I'm so afraid I can't do that in version 0.0.14");
   }
 }
