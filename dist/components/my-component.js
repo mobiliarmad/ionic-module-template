c0b228f57d98c869f38caf9963491bd097ad0ec4
loet <loet@swissonline.ch>
2017-11-03 14:21:49 +0100

    added gulp task for inlining html and css files

--- dist/components/my-component.js
+++ dist/components/my-component.js
@@ -9,7 +9,7 @@ MyComponent.decorators = [
     { type: Component, args: [{
                 selector: 'my-component',
                 template: "\n      <div class=\"myClass\">I'm a special snowflake</div>\n    ",
-                styles: ["\n      .myClass {\n        background-color: orchid;\n      }\n    "]
+                styles: ["\n      .myClass {\n        background-color: orchid;\n        color: white;\n      }\n    "]
             },] },
 ];
 /** @nocollapse */
